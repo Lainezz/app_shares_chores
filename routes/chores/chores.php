553bab0300a6fe7b1a4e32330822b8ab332fef73
Lainezz <diegolinaresmojacar@gmail.com>
2025-02-14 18:24:04 +0100

    adicion de protegida

--- routes/chores/chores.php
+++ routes/chores/chores.php
@@ -3,6 +3,20 @@
 use App\Http\Controllers\ChoreController;
 use Illuminate\Support\Facades\Route;
 
-// RUTA PARA ENRUTAR /user/
-Route::put('/{id}', [ChoreController::class, 'updateStatus'])->name('chore.updateStatus');
-Route::delete('/{id}', [ChoreController::class, 'delete'])->name('chore.delete');
+Route::middleware(['auth'])->group(function(){
+    Route::put('/{id}', [ChoreController::class, 'updateStatus'])->name('chore.updateStatus');
+    Route::delete('/{id}', [ChoreController::class, 'delete'])->name('chore.delete');
+    
+    // Prueba de ruta protegida /chores/rutaProtegida
+    Route::get('/rutaProtegida', function () {
+        return view('viewProtegida');
+    });
+});
+
+// Prueba de ruta no protegida /chores/rutaNoProtegida
+Route::get('/rutaNoProtegida', function () {
+    return view('viewNoProtegida');
+});
+
+
+
